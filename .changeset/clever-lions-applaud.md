4104a2374a922227e578a777009069cf7eac8aa9
xiaohuohumax <1915970986@qq.com>
2024-03-24 15:43:08 +0800

    扫描文件夹添加是否存在校验
    BUG修复: 遍历目标文件夹时可能存在文件不存导致任务失败

---
+++ .changeset/clever-lions-applaud.md
@@ -0,0 +1,5 @@
+---
+"format-files-by-ignores": patch
+---
+
+BUG 修复: 遍历目标文件夹时可能存在文件不存导致任务失败
--- src/util/folder.ts
+++ src/util/folder.ts
@@ -1,7 +1,7 @@
 import ignore from 'ignore';
 import fs from 'fs';
 import path from 'path';
-import { Uri } from 'vscode';
+import { Logger } from './logger';
 
 /**
  * 通过 ignore 文件排除文件
@@ -13,13 +13,17 @@ import { Uri } from 'vscode';
 export function filterFolderByignore(root: string, ext: string[], ignoreFileNames: string[]): string[] {
     const res: string[] = [];
 
+    if (!fs.existsSync(root)) {
+        return res;
+    }
+
     const filter = ignore({ allowRelativePaths: true }).add(ext);
 
     // 获取过滤规则
     for (const ignoreFileName of ignoreFileNames) {
-        const gitignore = path.resolve(root, ignoreFileName);
-        if (fs.existsSync(gitignore)) {
-            filter.add(fs.readFileSync(gitignore, 'utf-8'));
+        const ignoreFile = path.resolve(root, ignoreFileName);
+        if (fs.existsSync(ignoreFile)) {
+            filter.add(fs.readFileSync(ignoreFile, 'utf-8'));
         }
     }
 
@@ -27,6 +31,10 @@ export function filterFolderByignore(root: string, ext: string[], ignoreFileName
     const files = filter.filter(fs.readdirSync(root));
 
     for (const file of files.map(f => path.join(root, f))) {
+        // 文件可能不存在异常
+        if (!fs.existsSync(file)) {
+            continue;
+        }
         const fileStat = fs.statSync(file);
         if (fileStat.isDirectory()) {
             // 递归遍历
